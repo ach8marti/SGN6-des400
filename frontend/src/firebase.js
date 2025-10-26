235f439be20261fea2344972ccd036f53f245a94
ach8marti <bonusacharawan@gmail.com>
2025-10-26 14:02:06 +0700

    add .gitignore to remove key to public from frontend

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqulDlLOyU8CrNZTRwlqKZvmcxmSexcMg",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "tlm-des400.firebaseapp.com",
   projectId: "tlm-des400",
-  storageBucket: "tlm-des400.appspot.com", // fixed from .firebasestorage.app
+  storageBucket: "tlm-des400.appspot.com",
   messagingSenderId: "534169170503",
   appId: "1:534169170503:web:978d7224ba68cda6f87762",
   measurementId: "G-E8CCX27BR9",
