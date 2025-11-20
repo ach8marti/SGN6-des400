f7f660a69baea430f8ab5a6340a540ca3aecd3d9
TheMwan <punyawee.pou@gmail.com>
2025-11-20 14:12:25 +0700

    Add backend/frontend updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 //import Killer from "./Killer/Killer";
 //import IndexPage from "./IndexPage/IndexPage.jsx";
 //import Character from "./Character/Character.jsx";
-//import Investigate from "./Investigate/Investigate.jsx";
+import Investigate from "./Investigate/Investigate.jsx";
 //import BadEnd from "./Ending/BadEnd.jsx";
 //import Evidence from "./Evidence/Evidence.jsx";
 //import Unlock from "./Phone/Unlock.jsx";
 //import MessageApp from "./Phone/MessageApp.jsx";
-import Chat from "./Phone/Chat.jsx";
+//import Chat from "./Phone/Chat.jsx";
 
 export default function App() {
-  return <Chat />;
+  return <Investigate />;
 
 }
